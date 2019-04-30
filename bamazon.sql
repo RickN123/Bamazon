c0527c93472e4d5990eb08b614aa65733f312d68
RickN123 <nicolai.richard@gmail.com>
2019-04-30 17:58:19 -0400

    Added products

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,16 @@ price,
 stock_quantity,
 
 );
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Nike Airmax', 'Shoes', 75.00, 200),
+('Adidas Sweatshirt', 'Clothing', 38.00, 100),
+('Reebok Hat','Headwear', 24.00, 300),
+('Puma Shirt','Clothing', 28.00, 140),
+('Fila Classic','Shoes', 80.00, 400),
+('Jordan Dunks','Shoes', 90.00, 250),
+('Reebok Iversons','Shoes', 120.00, 500),
+('','',    ,     ),
+('','',    ,     ),
+('','',    ,     ),
