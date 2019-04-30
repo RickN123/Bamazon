ad4dbab3c29f43158ac6c040110441941d8657a5
RickN123 <nicolai.richard@gmail.com>
2019-04-30 17:46:29 -0400

    added items

--- bamazon.sql
+++ bamazon.sql
@@ -2,4 +2,12 @@ CREATE DATABASE bamazon;
 
 
 
-CREATE TABLE products
+CREATE TABLE products (
+
+item_id INTEGER (30),
+product_name VARCHAR (30),
+department_name VARCHAR (30),
+price,
+stock_quantity,
+
+);
