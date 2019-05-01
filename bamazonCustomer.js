b82cb81c7356f5dfd3c36d67d5403236e7807d28
RickN123 <nicolai.richard@gmail.com>
2019-04-30 23:46:33 -0400

    update table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,50 +4,82 @@ var table = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
-
     port: 3306,
-
     user: "root",
-
     password: "Barclays21",
     database: "bamazon"
-
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+
 });
 
-function connection.end(){
+function listProducts(callback) {
+
+    connection.query('select * from products', callback);
+}
+
+function promptForPurchase(callback) {
+
+
     inquirer.prompt([
         {
 
             type: "input",
             name: "item_id",
             message: "What is the ID of the product you would like to purchase?",
-            validate: validateInput,
             filter: Number
         },
         {
 
             type: "input",
             name: "stock_quantity",
-            message: "How many units would you like to purchase?",
-            validate: validateInput,
+            message: "How many clothing items would you like to purchase?",
             filter: Number
 
         }
 
 
-    ]).then(function (input) {
-        var item = input.item_id;
-        var quantity = input.quantity;
-
-
-
-    }
-
-
+    ]).then(callback);
 }
+
+listProducts(function (err, result, fields) {
+
+    console.table(result);
+
+    promptForPurchase(function (input) {
+        var item_id = input.item_id;
+        var stock_quantity = input.stock_quantity;
+
+
+        var currentStockSql = 'select stock_quantity from products where item_id = ' + item_id;
+
+        connection.query(currentStockSql, function (err, result, fields) {
+
+            if (err) console.log(err);
+
+            if (stock_quantity > result[0].stock_quantity) {
+                console.log('The amount you\'ve requested is more than what is in stock.\n\n');
+                return;
+            }
+
+            var updateProductSql = 'update products set stock_quantity = stock_quantity - ' + stock_quantity + ' where item_id = ' + item_id;
+
+            connection.query(updateProductSql, function (err, result, fields) {
+
+                if (err) console.log(err);
+
+                console.log(result.affectedRows + ' record(s) updated\n\n');
+
+                listProducts(function (err, result, fields) {
+                    console.table(result);
+                });
+            });
+        });
+
+    })
+
+});
+
